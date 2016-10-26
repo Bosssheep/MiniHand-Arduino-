0581f850790c69aeb0bea39e5b087cc07ef3d5e3
Bosssheep <389137297@qq.com>
2016-10-26 12:05:27 +0800

    Update motortest.ino
    add servocontrol

--- motortest.ino
+++ motortest.ino
@@ -1,13 +1,44 @@
+#include <Servo.h>
 
 #define IN1 4
-#define IN2  3
+#define IN2 3
 #define IN3 8
 #define IN4 12
+Servo servohand1;
+Servo servohand2;
+Servo servohead;
+Servo servoclaw;
+int r;
+int h;
+
 
 #define leftSpeed 100
 #define rightSpeed 100
 
+//电机初始化
+void IniMotor()
+{
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  //pinMode(ENA,OUTPUT);
+  //pinMode(ENB,OUTPUT);
+}
 
+//舵机初始化
+void iniServo()
+{
+  servoclaw.attach(3);
+  servohand2.attach(5);
+  servohead.attach(6);
+  servohead.attach(9);
+  servohand1.write(90);
+  servohand2.write(90);
+  servohead.write(90);
+  servoclaw.write(90);
+  
+}
 //直走
 void line()
 {
@@ -16,16 +47,7 @@ void line()
   digitalWrite(IN3,LOW);
   digitalWrite(IN4,HIGH);
 }
-void back()
-{
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,LOW);
-  delay(100);
-  digitalWrite(IN3,HIGH);
-  digitalWrite(IN4,LOW);
-  //analogWrite(ENA,rightSpeed);
-  //analogWrite(ENB,leftSpeed);
-}
+
 //右转
 void turnRight()
 {
@@ -44,55 +66,121 @@ void turnLeft()
   digitalWrite(IN3,LOW);
   digitalWrite(IN4,HIGH);
 }
+
 //制动
 void stopp()
 {
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,HIGH);
-  digitalWrite(IN3,HIGH);
-  digitalWrite(IN4,HIGH);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,LOW);
   //analogWrite(ENA,rightSpeed);
   //analogWrite(ENB,leftSpeed); 
 }
 
-
-void IniMotor()
+//拾起
+void pickup()
 {
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-  pinMode(IN3,OUTPUT);
-  pinMode(IN4,OUTPUT);
-  //pinMode(ENA,OUTPUT);
-  //pinMode(ENB,OUTPUT);
+    int i = servohand1.read();
+    for(i;i<=160;i++)
+    {
+        servohand1.write(i);
+        delay(30);
+    } 
+    int j = servohand2.read();
+    for(j;j>=78;j--)
+    {
+        servohand2.write(j);
+        delay(30);
+    }
+    int k = servoclaw.read();
+    for(k;k>=0;k--)
+    {
+        servoclaw.write(k);
+        delay(30);
+    }
 }
+
+//放下
+void putdown()
+{
+    int i = servohand1.read();
+    for(i;i<=160;i++)
+    {
+        servohand1.write(i);
+        delay(30);
+    } 
+    int j = servohand2.read();
+    for(j;j<=180;j++)
+    {
+        servohand2.write(j);
+        delay(30);
+    }
+    int k = servoclaw.read();
+    for(k;k>=150;k--)
+    {
+        servoclaw.write(k);
+        delay(30);
+    }
+}
+
+//机械臂转向左
+void switchleft()
+{
+    int p = servohead.read();
+    int q = p-15;
+    for(p;p>=q;p--)
+    {
+        servohead.write(p);
+        delay(30);
+    }
+}
+
+//机械臂转向右
+void switchright()
+{
+    int p = servohead.read();
+    int q = p+15;
+    for(p;p<=q;p++)
+    {
+        servohead.write(p);
+        delay(30);
+    }
+}
+
 
 void RunMotor() //run the motors
 {
-  int x;
-  x=Serial.read();
-  if(x=='1')
+  unsigned  char x;
+  if(Serial.available())
+  {x=Serial.read();
+  switch(x)
   {
-    line();
-    delay(100);
-  }
-  if(x=='2')
-  {
-    turnRight();
-    delay(100);
-  }
-  if(x=='3')
-  {
-    turnLeft();
-    delay(100);
-  }
-  if(x=='4')
-  {
-    stopp();
-    delay(100);
-  if(x=='5')
-  {
-    back();
-    delay(100);
+    case 0:
+          line();
+          break;
+    case 1:
+          turnRight();
+          break;
+
+    case 2:
+          turnLeft();
+          break;
+    case 3:
+          stopp();
+          break;
+    case 4:
+          pickup();
+          break;
+    case 5:
+          putdown();
+          break;
+    case 6:
+          switchleft();
+          break;
+    case 7:
+          switchright();
+          break;
   }
   }
 }
@@ -101,10 +189,20 @@ void RunMotor() //run the motors
 void setup() {
   IniMotor();
   Serial.begin(9600);
-
+  iniServo();
 }
 
 void loop() {
-  RunMotor();
+  if(Serial.available())
+  {
+    r = Serial.read();
+    if(r!=-1){
+      h=r;
+    }
+    switch(h)
+    {
+      
+    }
+  }
 
 }
